53c4281176c9c3da9b601bb070f6b3ee394f3ff2
Zoubair Entifi <zoentifi@e1r9p4.1337.ma>
2025-04-18 11:48:23 +0100

    ss

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,11 +6,12 @@
 /*   By: zoentifi <zoentifi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 09:17:14 by abenba            #+#    #+#             */
-/*   Updated: 2025/04/18 10:30:11 by zoentifi         ###   ########.fr       */
+/*   Updated: 2025/04/18 11:47:08 by zoentifi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "../minishell.h"
 
 static char	*ft_join(char *ptr, const char *s1, const char *s2)
 {
@@ -41,7 +42,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	if (!s1 || !s2)
 		return (NULL);
 	ptr_len = ft_strlen(s1) + ft_strlen(s2);
-	ptr = (char *)gc_malloc(ptr_len + 1);
+	ptr = gc_malloc(ptr_len + 1);
 	if (!ptr)
 		return (NULL);
 	if (ptr_len == 0)
