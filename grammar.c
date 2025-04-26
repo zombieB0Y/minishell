f369b21da9273534fad6b305ed8e831ac803af67
Zoubair Entifi <zoentifi@e2r11p6.1337.ma>
2025-04-26 20:35:53 +0100

    | or any opearator first, need to handel

--- grammar.c
+++ grammar.c
@@ -6,7 +6,7 @@
 /*   By: zoentifi <zoentifi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 16:59:23 by zoentifi          #+#    #+#             */
-/*   Updated: 2025/04/26 19:50:29 by zoentifi         ###   ########.fr       */
+/*   Updated: 2025/04/26 20:26:53 by zoentifi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,16 +89,18 @@ token_list_t	*grammar_check(token_list_t *tokens)
 				pos = pos->next;
 				i++;
 			}
-			else if (pos->token->type == TOKEN_PIPE)
-			{
-				
-			}
 			pos = head->next;
 			while (pos && pos->token->type == TOKEN_WORD)
 			{
 				remove_token_node(&tokens->head, pos);
 				tokens->size--;
 				pos = head->next;
+			}
+			if (pos->token->type == TOKEN_PIPE)
+			{
+				remove_token_node(&tokens->head, pos);
+				tokens->size--;
+				
 			}
 		}
 	token_list_print(tokens);
